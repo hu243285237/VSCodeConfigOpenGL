83435bd9397d5ca77d3ef8ed770a8ad96a0f8bc1
hu243285237 <243285237@qq.com>
2019-03-03 22:50:39 +0800

    Thanks everyone! Config VSCode success!

--- main.cpp
+++ main.cpp
@@ -1,42 +1,44 @@
     #include <iostream>
-    #include <glfw3.h>
-    // settings
-    const unsigned int SCR_WIDTH = 800;
-    const unsigned int SCR_HEIGHT = 600;
+    #include <GL/gl.h>
+    #include "glfw3.h"
+
     int main()
     {
-        // glfw: initialize and configure
-        // ------------------------------
-        glfwInit();
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    #ifdef __APPLE__
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //uncomment this statement to fix compilation on OS X
-    #endif
-        // glfw window creation
-        // --------------------
-        GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT,"LearnOpenGL", NULL, NULL);
-        if (window == NULL)
+        GLFWwindow* window;
+
+        if (!glfwInit()) return -1;
+
+        window = glfwCreateWindow(480, 320, "TestOpenGL", NULL ,NULL);
+
+        if (!window)
         {
-            std::cout << "Failed to create GLFW window" << std::endl;
             glfwTerminate();
             return -1;
         }
+
         glfwMakeContextCurrent(window);
-        // render loop
-        // -----------
+
         while (!glfwWindowShouldClose(window))
         {
-            glClearColor(0.0f, 1.f, 0.0f,1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
-            // glfw: swap buffers and poll IO events (keyspressed/released, mouse moved etc.)
-            // ---------------------------------------------------
+            glBegin(GL_TRIANGLES);
+
+            glColor3f(1.0, 0.0, 0.0);
+            glVertex3f(0.0, 1.0 ,0.0);
+
+            glColor3f(0.0, 1.0, 0.0);
+            glVertex3f(-1.0, -1.0 ,0.0);
+
+            glColor3f(1.0, 0.0, 1.0);
+            glVertex3f(1.0, -1.0 ,0.0);
+
+            glEnd();
+
             glfwSwapBuffers(window);
+
             glfwPollEvents();
         }
-        // glfw: terminate, clearing all previously allocated GLFWresources.
-        //---------------------------------------------------------------
+
         glfwTerminate();
+        
         return 0;
     }
